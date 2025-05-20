6cd8631da685b45a4b33e177df9adce62632b292
niharika464 <niharikasinghgaur11@gmail.com>
2025-05-20 21:45:21 +0530

    activity 3

---
+++ act3.py
@@ -0,0 +1,9 @@
+def number(n):
+    count = 0
+    while n:
+        count += 1
+        n >>= 1
+        print(count, "count the loops")
+        return
+n=int(input("Enter the number:"))
+number(n)    
